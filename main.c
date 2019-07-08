d6822fdac05bde2249c5b08c699d240743ef3c80
BMAN <554565808@qq.com>
2019-07-08 11:40:06 +0800

    bug

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 	// TODO: broadcast packet 
 	fprintf(stdout, "TODO: broadcast packet here.\n");
     iface_info_t * pos = NULL;
-	list_for_each_entry(pos, instance->iface_list, list) {
+	list_for_each_entry(pos, &instance->iface_list, list) {
         if(pos->fd != iface->fd && pos->index != iface->index) {
             iface_send_packet(pos, packet, len);
         }
